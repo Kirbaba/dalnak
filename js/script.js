61c4123f61071185e45e9679d33b517d0b7b24ca
gorotto <yastreb92@inbox.ru>
2016-07-14 16:49:18 +0300

    black-knife

--- js/script.js
+++ js/script.js
@@ -54,6 +54,12 @@
 
             // Зум по умолчанию. Возможные значения от 0 до 21
     };
+    var mapOptionsKnives = {
+        center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+        zoom: 8,
+
+            // Зум по умолчанию. Возможные значения от 0 до 21
+    };
     var mapOptionsOld = {
 
         center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
@@ -66,8 +72,31 @@
 
     // Создаем карту внутри элемента #map
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var map = new google.maps.Map(document.getElementById("map-old"), mapOptionsOld);
+    var mapOld = new google.maps.Map(document.getElementById("map-old"), mapOptionsOld);
+    var mapBlack = new google.maps.Map(document.getElementById("mapBlack"), mapOptionsKnives);
 
+}
+function initialize() {
+
+
+  // map.setOptions({styles: styles});
+  // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
+  // var centerLatLng = new google.maps.LatLng(55.74929, 37.0720767);
+  //
+  // // Обязательные опции с которыми будет проинициализированна карта
+  //
+ var myLatlng = new google.maps.LatLng(57.0442, 9.9116);
+ var mapOptionsKnives = {
+     center: myLatlng, // Координаты центра мы берем из переменной centerLatLng
+       zoom: 10,
+
+  };
+  var mapBlack = new google.maps.Map(document.getElementById("mapBlack"), mapOptionsKnives);
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title:"We are here!"
+  });
 }
 
 // Ждем полной загрузки страницы, после этого запускаем initMap()
