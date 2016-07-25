3b9288326fa2b5cb6e36f070bcee7963b4b58ef3
gorotto <yastreb92@inbox.ru>
2016-07-25 16:59:04 +0300

    travels

--- js/script.js
+++ js/script.js
@@ -1,69 +1,83 @@
-var map1, map2, map3;
-function initialize() {
-  var centerLatLng = new google.maps.LatLng(55.74929, 37.0720767);
-  var mapOptions = {
-      center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-      zoom: 8,
-  };
-  var styles = [
-    {
-      stylers: [
+var map, map_old, mapblack;
+ymaps.ready(function () {
+  map = new ymaps.Map("map", {
+          center: [59.94, 30.32],
+          zoom: 12
+      });
+  map = new ymaps.Map("map_old", {
+          center: [59.94, 30.32],
+          zoom: 12
+      });
+  map = new ymaps.Map("mapblack", {
+          center: [59.94, 30.32],
+          zoom: 12
+      });
 
-        { hue: "#000000" },
-        { saturation: -100 },
-         { visibility: "on" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        { lightness: 100 },
-        { visibility: "simplified" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "labels",
-      stylers: [
-        { visibility: "off" }
-      ]
-    },
-    {
-    featureType: "landscape",
-    elementType: "all",
-    stylers: [
-      { visibility: "off" },
-      { color: "#c7c7c7" },
-      { saturation: -100 },
-      { lightness: 20 }
-    ]
-  }
-  ];
-  var mapOptionsOld = {
-      center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-     disableDefaultUI: true,
-     styles: styles
-  };
+  });
 
-  var myLatlng3 = new google.maps.LatLng(57.0442, 9.9116);
-  var mapOptionsKnives = {
-       center: myLatlng3, // Координаты центра мы берем из переменной centerLatLng
-         zoom: 14,
-    };
-    if ($('#map').length > 0) {
-      map1 = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
+  // var centerLatLng = new ymaps.Map(55.74929, 37.0720767);
+  // var mapOptions = {
+  //     center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+  //     zoom: 8,
+  // };
+  // var styles = [
+  //   {
+  //     stylers: [
+  //
+  //       { hue: "#000000" },
+  //       { saturation: -100 },
+  //        { visibility: "on" }
+  //     ]
+  //   },{
+  //     featureType: "road",
+  //     elementType: "geometry",
+  //     stylers: [
+  //       { lightness: 100 },
+  //       { visibility: "simplified" }
+  //     ]
+  //   },{
+  //     featureType: "road",
+  //     elementType: "labels",
+  //     stylers: [
+  //       { visibility: "off" }
+  //     ]
+  //   },
+  //   {
+  //   featureType: "landscape",
+  //   elementType: "all",
+  //   stylers: [
+  //     { visibility: "off" },
+  //     { color: "#c7c7c7" },
+  //     { saturation: -100 },
+  //     { lightness: 20 }
+  //   ]
+  // }
+  // ];
+  // var mapOptionsOld = {
+  //     center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+  //     zoom: 8,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //    disableDefaultUI: true,
+  //    styles: styles
+  // };
+  //
+  // var myLatlng3 = new google.maps.LatLng(57.0442, 9.9116);
+  // var mapOptionsKnives = {
+  //      center: myLatlng3, // Координаты центра мы берем из переменной centerLatLng
+  //        zoom: 14,
+  //   };
 
-    if ($('#map-old').length > 0) {
-    map2 = new google.maps.Map(document.getElementById("map-old"), mapOptionsOld);
-    }
 
-    if ($('#mapBlack').length > 0) {
-    map3 = new google.maps.Map(document.getElementById("mapBlack"), mapOptionsKnives);
-    }
 
-}
+    // if ($('#map-old').length > 0) {
+    // map2 = new google.maps.Map(document.getElementById("map-old"), mapOptionsOld);
+    // }
+    //
+    // if ($('#mapBlack').length > 0) {
+    // map3 = new google.maps.Map(document.getElementById("mapBlack"), mapOptionsKnives);
+    // }
+
+
 
 
 
@@ -97,6 +111,10 @@ $(document).ready(function() {
     $('.pokazat-2').click(function(event) {
       event.preventDefault();
       $('.about-knives-hide').slideToggle();
+    });
+    $('.pokazat-map').click(function(event) {
+      event.preventDefault();
+      $('.hide-map').slideToggle();
     });
 });
 $(document).ready(function () {
